b06ed2adf290f9247aa2bcf29315399e3a991ac6
saghark <ksaghari0@gmail.com>
2016-12-01 04:39:00 +0000

    Contact form is now functional with database storage and a notice upon submission

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,19 @@
 class ContactsController < ApplicationController
-    def home
-        
+    def new
+        @contact = Contact.new
     end
     
-    def about
-    
+    def create
+       @contact = Contact.new(contact_params)
+       if @contact.save
+           redirect_to new_contact_path, notice: "Message Sent!"
+       else
+           redirect_to new_contact_path, notice: "Message Not Sent :-("
+       end
     end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
