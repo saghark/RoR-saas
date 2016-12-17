fc8daa6e221136cc0874e8c719dff68d2152c3b7
saghark <ksaghari0@gmail.com>
2016-12-17 03:51:02 +0000

    Finished mailer for the contact form. Heroku projecrt created.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,16 +6,24 @@ class ContactsController < ApplicationController
     def create
        @contact = Contact.new(contact_params)
        if @contact.save
-         flash[:success] = "Message Sent!"
-           redirect_to new_contact_path
+        # Send email when the contact form is successfully saved.
+        name = params[:contact][:name]
+        email = params[:contact][:email]
+        body = params[:contact][:comments]
+        ContactMailer.contact_email(name, email, body).deliver
+        # Flash a message to the user letting them know the message was sent.
+        flash[:success] = "Message Sent!"
+        redirect_to new_contact_path
        else
-         flash[:danger] = @contact.errors.full_messages.join(", ")
-           redirect_to new_contact_path
+         # Flash a message to the user letting the know the message wasn't sent.
+         # Also show any possible errors.
+        flash[:danger] = @contact.errors.full_messages.join(", ")
+        redirect_to new_contact_path
        end
     end
     
     private
         def contact_params
-            params.require(:contact).permit(:name, :email, :comments)
+          params.require(:contact).permit(:name, :email, :comments)
         end
 end
